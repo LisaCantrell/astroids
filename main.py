2cd19cc8eb6674ea248414008084aedd6976078b
LisaCantrell <f.panda@gmail.com>
2024-09-05 18:19:18 -0500

    add game loop and create a surface

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import pygame
+from constants import *
 
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    my_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fill_color_black = (0,0,0)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        my_surface.fill(fill_color_black)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
