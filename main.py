9c159dc58fc8b463d19e5389654905d4c27ed9e3
LisaCantrell <f.panda@gmail.com>
2024-09-06 15:04:19 -0500

    add player class, including drawing player sprite on screen, and turning sprite left and right

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     game_clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    my_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    fill_color_black = (0,0,0)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        my_surface.fill(fill_color_black)
-        pygame.display.flip()
         dt = (game_clock.tick(60))/1000
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
